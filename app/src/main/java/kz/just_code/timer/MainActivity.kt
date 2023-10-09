a3f8ae15dde0e3fea392ce11b108c394c4799e61
Madina <123440519+sleepingforest100@users.noreply.github.com>
2023-10-09 12:26:25 +0600

    new functions

--- app/src/main/java/kz/just_code/timer/MainActivity.kt
+++ app/src/main/java/kz/just_code/timer/MainActivity.kt
@@ -6,9 +6,11 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kz.just_code.timer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
